d2c16e184ce13205354a7b05ba6b17ef642db33b
mickeyyo55b <mickeyyo55b@gmail.com>
2025-08-04 15:39:35 +0800

    08041539

--- src/main/java/tw/brad/spring6/dto/OrderDTO.java
+++ src/main/java/tw/brad/spring6/dto/OrderDTO.java
@@ -1,8 +1,44 @@
 package tw.brad.spring6.dto;
 
 import java.util.Date;
+import java.util.List;
 
 public class OrderDTO {
 	private Long orderId;
 	private Date orderDate;
+	private List<OrderDetailDTO> details;
+	
+	public OrderDTO(
+			Long orderId,
+			Date orderDate,
+			List<OrderDetailDTO> details
+			) {
+		this.orderId = orderId;
+		this.orderDate = orderDate;
+		this.details = details;
+	}
+	
+	
+	public Long getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	public List<OrderDetailDTO> getDetails() {
+		return details;
+	}
+	public void setDetails(List<OrderDetailDTO> details) {
+		this.details = details;
+	}
+	
+	
+	
+	
 }
---
+++ src/main/java/tw/brad/spring6/dto/OrderDetailDTO.java
@@ -0,0 +1,41 @@
+package tw.brad.spring6.dto;
+
+import java.math.BigDecimal;
+
+public class OrderDetailDTO {
+	private BigDecimal unitPrice;
+	private Integer quantity;
+	private String productName;
+	
+	public OrderDetailDTO(
+			BigDecimal unitPrice,
+			Integer quantity,
+			String productName
+			) {
+		this.unitPrice = unitPrice;
+		this.quantity = quantity;
+		this.productName = productName;
+	}
+	
+	
+	public BigDecimal getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(BigDecimal unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	
+	
+}
