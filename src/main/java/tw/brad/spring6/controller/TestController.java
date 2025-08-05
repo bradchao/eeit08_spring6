066449936b292933c91e1c76cdf00d1816263a19
mickeyyo55b <mickeyyo55b@gmail.com>
2025-08-05 11:05:54 +0800

    08051105

--- src/main/java/tw/brad/spring6/controller/TestController.java
+++ src/main/java/tw/brad/spring6/controller/TestController.java
@@ -1,9 +1,16 @@
 package tw.brad.spring6.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import tw.brad.spring6.aop.CheckJwt;
 import tw.brad.spring6.utils.JwtTool;
 
 @RestController
@@ -21,6 +28,47 @@ public class TestController {
 		String email = JwtTool.parseToken(token);
 		System.out.println(email);
 	}
+	
+	/*
+	 * req : {
+	 * 	email : "",
+	 * 	passwd : ""
+	 * }
+	 * 
+	 * resp: {
+	 * 	token: "....."
+	 * }
+	 */
+	@PostMapping("/member/login")
+	public ResponseEntity<Map<String,String>> login(@RequestBody Map<String,String> data) {
+		String email = data.get("email");
+		String passwd = data.get("passwd");
+		
+		Map<String,String> result = new HashMap<>();
+		if (email.equals("brad@brad.tw") && passwd.equals("123456")) {
+			result.put("token", JwtTool.createToken(email));
+		}
+		
+		return ResponseEntity.ok(result);
+	}
+	
+	@CheckJwt
+	@PostMapping("/member/status")
+	public ResponseEntity<Map<String,String>> status() {
+		System.out.println("Welcome");
+		Map<String,String> result = Map.of("status","ok");
+		return ResponseEntity.ok(result);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
